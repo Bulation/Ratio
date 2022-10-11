3773f20a748f511e17399bce0c67809eda4b95ad
Bulation <insanbulat@gmail.com>
2022-10-11 22:07:48 +0300

    add constants

---
+++ src/ts/constants/constants.ts
@@ -0,0 +1,26 @@
+export const BOARD_SIZE = 5;
+export const PROBABILITY = 0.9;
+export const MIN_DEFAULT_NUM = 2;
+export const MAX_DEFAULT_NUM = 4;
+export const NUMBERS_ON_TOP_OF_BOARD = [0, 1, 2, 3, 4];
+export const NUMBERS_ON_RIGHT_OF_BOARD = [4, 9, 14, 19, 24];
+export const NUMBERS_ON_BOTTOM_OF_BOARD = [20, 21, 22, 23, 24];
+export const NUMBERS_ON_LEFT_OF_BOARD = [0, 5, 10, 15, 20];
+export const DEFAULT_FONT_SIZE = 40;
+export const FONT_COEF = 5;
+export const STATE_KEYS = ['bestScore', 'score', 'time', 'board', 'boardsStack', 'scoresStack', 'winValue', 'isWin', 'results', 'records'];
+export const COLORS = {
+  '2': 'rgb(230, 230, 250)',
+  '4': 'rgb(254, 222, 0)',
+  '8': 'rgb(108, 119, 159)',
+  '16': 'rgb(239, 60, 64)',
+  '32': 'rgb(39, 252, 62)',
+  '64': 'rgb(108, 143, 157)',
+  '128': 'rgb(222, 118, 49)',
+  '256': 'rgb(56, 119, 9)',
+  '512': 'rgb(12, 130, 183)',
+  '1024': 'rgb(237, 197, 63)',
+  '2048': 'rgb(153, 4, 254)',
+  '4096': 'rgb(250, 88, 73)'
+}
+export const WIN_VALUE = 2048;
