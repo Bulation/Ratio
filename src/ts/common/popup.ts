21f6dc49d5ab1b9f7b1458c89a48deece5b43a93
Bulation <insanbulat@gmail.com>
2022-10-11 22:07:20 +0300

    add class that describe popup

--- src/ts/common/popup.ts
+++ src/ts/common/popup.ts
@@ -2,19 +2,22 @@ import Component from './component';
 
 export default class Popup extends Component {
   overlay: Component;
-
-  closeButton: Component;
-
+  title: Component<HTMLElement>;
   constructor(
     parent: HTMLElement,
     tagName: keyof HTMLElementTagNameMap,
     className: string,
     content: string,
-    popupData: string,
+    popupContent: string
   ) {
     super(parent, tagName, className, content);
-    this.overlay = new Component(this.node, 'div', 'popup-wrapper', popupData);
-    this.closeButton = new Component(this.overlay.node, 'button', 'popup-button', 'x');
-    this.closeButton.setListener('click', () => this.node.remove());
+    this.overlay = new Component(this.node, 'div', 'popup-wrapper popup-animated', '');
+    this.title = new Component(this.overlay.node, 'h2', '', popupContent);
+    this.overlay.setListener('animationend', () => this.overlay.removeClass('popup-animated'));
+  }
+
+  destroy(): void {
+    this.overlay.setListener('animationend', () => this.node.remove());
+    this.overlay.setClass('popup-animate-out');
   }
 }
