a0e6ab19411db26cd91b551ce06c9d46330ea433
Bulation <insanbulat@gmail.com>
2023-04-08 13:23:22 +0300

    feat: add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,17 @@
+export const BASE_URL = 'https://course.7t33n.ru/rest/v1';
+export const MENU_ITEMS_COUNT = 3;
+export const ARTICLES_COUNT_PER_PAGE = 3;
+export const MENU_LINKS_OBJECT = [
+  {
+    content: 'Home',
+    url: '/',
+  },
+  {
+    content: 'Blog',
+    url: '/blog',
+  },
+  {
+    content: 'About',
+    url: '/about',
+  },
+];
