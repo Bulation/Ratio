63d69e79c0869458bf2a6ead87d8b30e26b560ca
Bulation <insanbulat@gmail.com>
2023-04-08 13:26:32 +0300

    feat: add page class

---
+++ src/pages/Page.ts
@@ -0,0 +1,8 @@
+import { IAbout } from '../interfaces/IAbout';
+import { IArticle } from '../interfaces/IArticle';
+import { IDetailedArticle } from '../interfaces/IDetailedArticle';
+
+export default abstract class Page {
+  abstract render(...params: Array<IAbout | IDetailedArticle | IArticle | IArticle[]>): void;
+  abstract removePage(): void;
+}
