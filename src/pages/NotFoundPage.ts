4167d84ab152a34bc200e8166d157680948b9b9d
Bulation <insanbulat@gmail.com>
2023-04-08 13:26:50 +0300

    feat: add not found page

---
+++ src/pages/NotFoundPage.ts
@@ -0,0 +1,22 @@
+import Component from '../common/component';
+import Page from './Page';
+
+export default class NotFoundPage extends Page {
+  content: Component;
+  parent: HTMLElement;
+  main: Component;
+
+  constructor(parent: HTMLElement) {
+    super();
+    this.parent = parent;
+  }
+
+  removePage() {
+    this.main.destroy();
+  }
+
+  render() {
+    this.main = new Component(this.parent, 'main', '', '');
+    this.content = new Component(this.main.node, 'span', '', '404');
+  }
+}
