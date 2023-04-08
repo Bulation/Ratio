17fff4e99b863f1855af38d245c7bacb5bb2d3e1
Bulation <insanbulat@gmail.com>
2023-04-08 13:29:38 +0300

    feat: add router for handling urls

---
+++ src/router.ts
@@ -0,0 +1,45 @@
+export default class Router {
+  routes: { [key: string]: (url: string) => boolean } = {};
+
+  addRoute(path: string, handleRoute: (params?: { [key: string]: string }) => void) {
+    const urlSegments = path.split('/');
+    const dynamicRoutes: string[] = [];
+    const regexPath = urlSegments
+      .map((segment) => {
+        if (segment.startsWith(':')) {
+          dynamicRoutes.push(segment.slice(1));
+          return `([^/]+)`;
+        } else {
+          return segment;
+        }
+      })
+      .join('/');
+    const regex = new RegExp(`^${regexPath}$`);
+    this.routes[path] = (url: string) => {
+      const match = url.match(regex);
+      if (match) {
+        const params: { [key: string]: string } = {};
+        dynamicRoutes.forEach((route, i) => {
+          params[route] = match[i + 1];
+        });
+        handleRoute(params);
+        return true;
+      }
+      return false;
+    };
+  }
+
+  navigate(url: string) {
+    let isWrongUrl = true;
+    Object.keys(this.routes).forEach((route) => {
+      const handler = this.routes[route];
+      if (handler(url)) {
+        isWrongUrl = false;
+      }
+    });
+    if (isWrongUrl) {
+      history.pushState('', '', '/404');
+      this.routes['/404']('/404');
+    }
+  }
+}
