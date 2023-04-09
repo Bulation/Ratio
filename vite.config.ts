9a9f1fc8be01294e7051a5dfec1b07aa4fc5dce7
Bulation <insanbulat@gmail.com>
2023-04-09 22:40:58 +0300

    feat: add url in environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/module4/',
+  base: process.env.NODE_ENV === 'production' ? '/Ratio/module4/' : '/',
+  define: {
+    'import.meta.env.SITE_URL': JSON.stringify(
+      process.env.NODE_ENV === 'production' ? '/Ratio/module4/' : '/'
+    ),
+  },
 });
