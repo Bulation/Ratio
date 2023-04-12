97d080fbca88356917a7daaea1089f79fbebf98a
Bulation <insanbulat@gmail.com>
2023-04-12 15:18:10 +0300

    docs: add comments

--- src/router.ts
+++ src/router.ts
@@ -5,41 +5,45 @@ export default class Router {
 
   addRoute(path: string, handleRoute: (params?: { [key: string]: string }) => void) {
     const urlSegments = path.split('/');
-    const dynamicRoutes: string[] = [];
+    const dynamicRoutes: string[] = []; // массив из динамических роутов. Если приходит урл вида /blog/:id/:num, в нем будут id и num
     const regexPath = urlSegments
       .map((segment) => {
         if (segment.startsWith(':')) {
           dynamicRoutes.push(segment.slice(1));
-          return `([^/]+)`;
+          return `([^/]+)`; // если маршрут динамический, то возвращаем скобочную группу внутри которой будет любой символ кроме / и сегмент пушим в массив роутов
         } else {
           return segment;
         }
       })
-      .join('/');
+      .join('/'); // получаем строку для создания регулярного выражения
+    // для маршрута вида /blog регуляркой будет ^/blog$, для /blog/articles/:id - ^/blog/articles/([^/]+)$
     const regex = new RegExp(`^${regexPath}$`);
     this.routes[path] = (url: string) => {
       const match = url.match(regex);
       if (match) {
-        const params: { [key: string]: string } = {};
+        const params: { [key: string]: string } = {}; // объект, который будет содержать значения для всех динамических роутов
         dynamicRoutes.forEach((route, i) => {
-          params[route] = match[i + 1];
+          // если параметр path содержал динамические роуты, то будем пробегаться по сохраненным в массив роутам и в объект params добавлять значения для динамических роутов
+          params[route] = match[i + 1]; // стоит i + 1, так как для регулярок со скобочными группами в 0 элементе массива match будет все совпадение, а в последующих элементах совпадения со скобочными группами
         });
         handleRoute(params);
-        return true;
       }
-      return false;
+      return Boolean(match); // если урл соответствует регулярке, то возвращаем тру иначе фолс
     };
   }
 
   navigate(url: string) {
-    let isWrongUrl = true;
+    let isWrongUrl = true; // переменная, отвечающая за то, является ли параметр url верным урлом
     Object.keys(this.routes).forEach((route) => {
+      // пробегаемся по всем роутам и вызываем все функции, содержащиеся в объекте this.routes
       const handler = this.routes[route];
       if (handler(url)) {
+        // если роут подходит под пришедший url, то значит url верный
         isWrongUrl = false;
       }
     });
     if (isWrongUrl) {
+      // если url неверный, то перенаправляем на страницу 404
       history.pushState('', '', `${SITE_URL}404`);
       this.routes[`${SITE_URL}404`](`${SITE_URL}404`);
     }
