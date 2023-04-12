eeecf95ecc9bee626f6199436f7ad5d4df59f20d
Bulation <insanbulat@gmail.com>
2023-04-12 16:17:12 +0300

    docs: add comments

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,9 @@
 export const BASE_URL = 'https://course.7t33n.ru/rest/v1';
-export const SITE_URL = import.meta.env.BASE_URL;
-export const MENU_ITEMS_COUNT = 3;
-export const ARTICLES_COUNT_PER_PAGE = 3;
+export const SITE_URL = import.meta.env.BASE_URL; // получение адреса сайта из env переменной
+export const MENU_ITEMS_COUNT = 3; // количество пунктов в меню
+export const ARTICLES_COUNT_PER_PAGE = 3; // количество статей на домашней странице
 export const MENU_LINKS_OBJECT = [
+  // массив объектов с контентом и урл для пунктов меню
   {
     content: 'Home',
     url: SITE_URL,
