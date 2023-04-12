9ac09348c1f1e8e6bf8aad3144abef2633853225
Bulation <insanbulat@gmail.com>
2023-04-12 16:19:59 +0300

    docs: add comments

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/Ratio/module4/' : '/',
   define: {
     'import.meta.env.SITE_URL': JSON.stringify(
+      // в import.meta.env.SITE_URL будем добавлять адрес сайта в зависимости от того, находимя ли в девелопмент моде или в продакшн
       process.env.NODE_ENV === 'production' ? '/Ratio/module4/' : '/'
     ),
   },
