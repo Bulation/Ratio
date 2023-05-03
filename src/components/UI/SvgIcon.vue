4589869a27fa518fb86efee8fc1252e19024bfdc
Bulation <insanbulat@gmail.com>
2023-05-03 17:55:12 +0300

    feat; add svg component

---
+++ src/components/UI/SvgIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+interface ISvgProps {
+  className: string,
+  id: string
+}
+defineProps<ISvgProps>();
+</script>
+
+<template>
+  <svg :class="className">
+    <use :href="`/svg/sprite.svg#${id}`" />
+  </svg>
+</template>
