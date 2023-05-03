38addaf9ed7e3a3cfe639e158d44e9977b50c0bc
Bulation <insanbulat@gmail.com>
2023-05-03 17:50:42 +0300

    feat: add cards list

---
+++ src/components/CardList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { IFeaturedData } from '@/interfaces/IFeaturedData';
+import type { ILatestData } from '@/interfaces/ILatestData';
+import CardItem from './CardItem.vue';
+
+interface ICardListProps {
+  list: ILatestData[] | IFeaturedData[]
+}
+defineProps<ICardListProps>()
+
+</script>
+
+<template>
+  <ul class="card-list">
+    <CardItem :list="list" location="latest" />
+  </ul>
+</template>
+
+<style scoped lang="scss">
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    column-gap: 30px;
+    row-gap: 70px;
+    @media screen and (max-width: 768px) {
+      justify-content: center;
+    }
+  }
+</style>
