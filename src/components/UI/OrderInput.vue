a34c32bfa074e9342906535e67ec88845daa5092
Bulation <insanbulat@gmail.com>
2023-05-03 22:57:08 +0300

    feat: add handler

--- src/components/UI/OrderInput.vue
+++ src/components/UI/OrderInput.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
-defineEmits(['input'])
-defineProps<{
+const emit = defineEmits(['input'])
+const props = defineProps<{
   type: string,
   placeholder: string,
   isRequired: boolean,
   inputValue: string,
   inputKey: string,
-  isError: boolean,
-  errorMessage: string
+  isError?: boolean,
+  errorMessage?: string
 }>()
+
+function handleInput(event: Event) {
+  const value = (event.target as HTMLInputElement).value;
+  emit('input', value, props.inputKey);
+}
 </script>
 
 <template>
   <div class="order-form-input-wrap">
-    <input class="order-form-input" :type="type" :value="inputValue" @input="$emit('input', $event.target.value, inputKey)" :placeholder="placeholder" :required="isRequired">
+    <input class="order-form-input" :type="type" :value="inputValue" @input="handleInput" :placeholder="placeholder" :required="isRequired">
     <span class="order-form-error-msg" v-if="isError">{{ errorMessage }}</span>
   </div>
 </template>
