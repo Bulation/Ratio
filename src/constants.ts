091469f0fb3e45da936491cef20175585bbed16c
Bulation <insanbulat@gmail.com>
2023-05-03 18:00:26 +0300

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,92 @@
+export const MENU_LINKS_OBJECT = [
+  {
+    content: 'Find a Property',
+    url: '#'
+  },
+  {
+    content: 'Share Stories',
+    url: '#'
+  },
+  {
+    content: 'Rental Guides',
+    url: '#'
+  },
+  {
+    content: 'Download Mobile App',
+    url: '#'
+  }
+];
+
+export const BASE_URL = 'https://module5.vue.panfilov.academy';
+export const COMPANY_LIST = [
+  {
+    value: 'About Us',
+    path: '#'
+  },
+  {
+    value: 'Legal Information',
+    path: '#'
+  },
+  {
+    value: 'Contact Us',
+    path: '#'
+  },
+  {
+    value: 'Blogs',
+    path: '#'
+  }
+];
+export const HELP_LIST = [
+  {
+    value: 'Find a Property',
+    path: '#'
+  },
+  {
+    value: 'How To Host?',
+    path: '#'
+  },
+  {
+    value: 'Why Us?',
+    path: '#'
+  },
+  {
+    value: 'FAQs',
+    path: '#'
+  },
+  {
+    value: 'Rental Guides',
+    path: '#'
+  }
+];
+export const CONTACT_LIST = [
+  {
+    value: 'Phone: 1234567890',
+    path: 'tel:+1234567890'
+  },
+  {
+    value: 'Email: company@email.com',
+    path: 'mailto:company@email.com'
+  },
+  {
+    value: 'Location: 100 Smart Street, LA, USA',
+    path: 'https://goo.gl/maps/cAEmoUonSrsjoapw9'
+  }
+];
+export const SOCIALS_LIST = [
+  {
+    iconId: 'fb',
+    path: 'https://facebook.com'
+  },
+  {
+    iconId: 'twitter',
+    path: 'https://twitter.com'
+  },
+  {
+    iconId: 'insta',
+    path: 'https://instagram.com'
+  },
+  {
+    iconId: 'linkedin',
+    path: 'https://linkedin.com'
+  }
+];
