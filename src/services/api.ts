5088e979f61e4ac4a2ada31d6bb593098fe263e3
Bulation <insanbulat@gmail.com>
2023-05-03 22:58:35 +0300

    refactor: rename interfaces

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,63 +1,65 @@
 import { BASE_URL } from "@/constants"
 import type { IBannerData } from "@/interfaces/IBannerData";
 import type { ICountry } from "@/interfaces/ICountry";
-import type { IFeaturedData } from "@/interfaces/IFeaturedData";
+import type { IHotelData } from '@/interfaces/IHotelData'
 import type { IFilter } from "@/interfaces/IFilter";
-import type { IForm } from "@/interfaces/IForm";
-import type { IHotel } from "@/interfaces/IHotel";
-import type { ILatestData } from "@/interfaces/ILatestData";
+import type { IOrderForm } from "@/interfaces/IOrderForm";
+import type { IDetailedHotelData } from '@/interfaces/IDetailedHotelData'
+import type { ILatestHotelData } from '@/interfaces/ILatestHotelData'
 
 const API = {
   async fetchData(path: string) {
-    const response = await fetch(`${BASE_URL}${path}`);
+    const response = await fetch(`${BASE_URL}${path}`)
     if (response.status >= 400 && response.status <= 600) {
-      throw new Error('Error');
+      throw new Error('Error')
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   },
 
-  async postData(path: string, body: IFilter | IForm) {
-    await fetch(`${BASE_URL}${path}`, {
+  async postData(path: string, body: IFilter | IOrderForm) {
+    const response = await fetch(`${BASE_URL}${path}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
     })
+    const data = await response.json()
+    return data
   },
 
   async getBanners(): Promise<IBannerData> {
-    const data = await this.fetchData('/banners');
-    return data;
+    const data = await this.fetchData('/banners')
+    return data
   },
 
-  async getFeaturedData(): Promise<IFeaturedData[]> {
+  async getFeaturedData(): Promise<IHotelData[]> {
     const data = await this.fetchData('/hotel/featured')
-    return data;
+    return data
   },
 
-  async getLatestData(): Promise<ILatestData[]> {
+  async getLatestData(): Promise<ILatestHotelData[]> {
     const data = await this.fetchData('/hotel/latest')
-    return data;
+    return data
   },
 
-  async getHotelData(id: string): Promise<IHotel> {
+  async getHotelData(id: string): Promise<IDetailedHotelData> {
     const data = await this.fetchData(`/hotel/detail/${id}`)
-    return data;
+    return data
   },
 
   async getCountries(): Promise<ICountry[]> {
     const data = await this.fetchData('/hotel/location')
-    return data;
+    return data
   },
 
   async postFilter(body: IFilter) {
-    await this.postData('/hotel/filter', body);
+    return this.postData('/hotel/filter', body)
   },
 
-  async postOrder(body: IForm) {
-    await this.postData('/order', body);
+  async postOrder(body: IOrderForm) {
+    return this.postData('/order', body)
   }
 }
 
