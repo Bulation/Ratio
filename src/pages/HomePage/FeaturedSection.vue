1992d811df3cb07e37c62e585d1b1aa2dd8fd079
Bulation <insanbulat@gmail.com>
2023-05-03 17:56:48 +0300

    feat: add featured section

---
+++ src/pages/HomePage/FeaturedSection.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { IFeaturedData } from '@/interfaces/IFeaturedData';
+import CardList from '@/components/CardList.vue';
+
+interface IFeaturedProps {
+  featuredList: IFeaturedData[]
+}
+defineProps<IFeaturedProps>();
+</script>
+
+<template>
+  <section class="featured">
+    <div class="featured__container">
+      <h2 class="section-title featured__title">Featured Properties on our Listing</h2>
+      <CardList :list="featuredList" />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .featured__container {
+    max-width: 1206px;
+    width: calc(100% - 30px);
+    margin: 0 auto;
+    padding: 20px 0 64px;
+  }
+
+  .featured__title {
+    max-width: 412px;
+    margin-bottom: 83px;
+  }
+</style>
