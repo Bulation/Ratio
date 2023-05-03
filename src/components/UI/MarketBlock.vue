158fb88279f969034199af9e80ace5bd39efd433
Bulation <insanbulat@gmail.com>
2023-05-03 17:59:19 +0300

    feat: add component for markets

---
+++ src/components/UI/MarketBlock.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import SvgIcon from './SvgIcon.vue';
+interface IMarketBlockProps {
+  path: string
+  iconClass: string,
+  iconId: string,
+  marketName: string
+}
+defineProps<IMarketBlockProps>()
+</script>
+
+<template>
+  <a :href="path" class="market">
+    <SvgIcon :className="iconClass" :id="iconId" />
+    <span class="market__name"> {{ marketName }}</span>
+  </a>
+</template>
+
+<style scoped lang="scss">
+  .market {
+    display: flex;
+    align-items: center;
+    padding: 15px 30px;
+    gap: 15px;
+    border-radius: 6px;
+    background-color: var(--market-bg);
+    &__name {
+      color: var(--main-text-color);
+      font: 600 15px/18px Montserrat;
+    }
+    &__icon {
+      width: 22px;
+      height: 22px;
+    }
+  }
+</style>
