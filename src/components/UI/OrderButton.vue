36c88f6a402033548394ff22612886e92dff427d
Bulation <insanbulat@gmail.com>
2023-05-03 19:39:44 +0300

    feat: add ui components for order page

---
+++ src/components/UI/OrderButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  isDisabled: boolean
+}>()
+</script>
+
+<template>
+  <button :disabled="isDisabled" class="order-btn" type="submit"><slot></slot></button>
+</template>
+
+<style lang="scss">
+  .order-btn {
+    font: 700 15px/18px Montserrat;
+    width: 324px;
+    padding: 22px 0;
+    border-radius: 30px;
+    color: #FFFFFF;
+    background-color: var(--button-color);
+    cursor: pointer;
+    &:disabled {
+      background-color: grey;
+    }
+  }
+</style>
---
+++ src/components/UI/OrderInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineEmits(['input'])
+defineProps<{
+  type: string,
+  placeholder: string,
+  isRequired: boolean,
+  inputValue: string,
+  inputKey: string,
+  isError: boolean,
+  errorMessage: string
+}>()
+</script>
+
+<template>
+  <div class="order-form-input-wrap">
+    <input class="order-form-input" :type="type" :value="inputValue" @input="$emit('input', $event.target.value, inputKey)" :placeholder="placeholder" :required="isRequired">
+    <span class="order-form-error-msg" v-if="isError">{{ errorMessage }}</span>
+  </div>
+</template>
+
+<style>
+  .order-form-input-wrap {
+    width: calc(50% - 7px);
+  }
+  .order-form-input {
+    width: 100%;
+    color: var(--order-input-color);
+    font: 500 14px/20px Montserrat;
+    padding: 15px;
+    border-bottom: 1px solid #000000;
+  }
+  .order-form-error-msg {
+    color: red;
+    font: 700 12px/15px Montserrat;
+  }
+</style>
