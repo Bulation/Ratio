4a96f79ed227234783599e02ba2bfa382db20d84
Bulation <insanbulat@gmail.com>
2023-05-07 13:23:54 +0300

    feat: add meta

--- src/pages/OrderPage/OrderPage.vue
+++ src/pages/OrderPage/OrderPage.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-import OrderForm from '@/components/OrderForm.vue';
-import OrderHotel from '@/components/OrderHotel.vue';
-import PageFooter from '@/layout/PageFooter.vue';
+import OrderForm from '@/components/OrderForm.vue'
+import OrderHotel from '@/components/OrderHotel.vue'
+import PageFooter from '@/layout/PageFooter.vue'
+import { useMeta } from 'vue-meta';
+useMeta({
+  title: "Order",
+  meta: [
+    { name: 'description', content: "Make order and preserve hotel" },
+    { name: 'keywords', content: "hotel, hotel list, order, preserve hotel" }
+  ],
+  og: {
+    title: "Order",
+    keywords: "hotel, hotel list, order, preserve hotel",
+    description: "Make order and preserve hotel",
+  },
+});
 </script>
 
 <template>
