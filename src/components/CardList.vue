995d3f9dff72dfa59c7b8434b65736412cbbfd17
Bulation <insanbulat@gmail.com>
2023-05-07 22:53:00 +0300

    feat: add transition group

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
-import type { IHotelData } from '@/interfaces/IHotelData';
-import type { ILatestHotelData } from '@/interfaces/ILatestHotelData';
-import CardItem from './CardItem.vue';
+import type { IHotelData } from '@/interfaces/IHotelData'
+import type { ILatestHotelData } from '@/interfaces/ILatestHotelData'
+import CardItem from './CardItem.vue'
+import type { IDetailedHotelData } from '@/interfaces/IDetailedHotelData'
 
 interface ICardListProps {
-  list: ILatestHotelData[] | IHotelData[]
+  list: ILatestHotelData[] | IHotelData[] | IDetailedHotelData[]
+  location: 'featured' | 'latest' | 'details'
 }
 defineProps<ICardListProps>()
-
 </script>
 
 <template>
   <ul class="card-list">
-    <CardItem :list="list" location="latest" />
+    <TransitionGroup  name="list">
+      <CardItem v-for="item in list" :key="item._id" :item="item" :location="location"  />
+    </TransitionGroup>
   </ul>
 </template>
 
-<style scoped lang="scss">
-  .card-list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    column-gap: 30px;
-    row-gap: 70px;
-    @media screen and (max-width: 768px) {
-      justify-content: center;
-    }
+<style lang="scss">
+.card-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  column-gap: 30px;
+  row-gap: 70px;
+  @media screen and (max-width: 768px) {
+    justify-content: center;
   }
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 </style>
