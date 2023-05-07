c93acc84c7eba65a1e5dc2e0b1a3c70feb1a7bdb
Bulation <insanbulat@gmail.com>
2023-05-07 13:29:14 +0300

    feat: add loader component

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,45 +1,58 @@
 <script setup lang="ts">
-import type { IBanner } from '@/interfaces/IBanner';
+import type { IBannerData } from '@/interfaces/IBannerData'
+import { ElSkeletonItem } from 'element-plus'
+import LoaderComponent from './LoaderComponent.vue';
 
 interface IBannerProps {
-  bannerData: IBanner
+  bannerData: IBannerData
+  bannerKey: keyof IBannerData
 }
-defineProps<IBannerProps>();
+defineProps<IBannerProps>()
+
 </script>
 
 <template>
   <section class="banner">
-    <div class="banner__container" :style="{ backgroundImage: `url(${bannerData.image})`}">
-      <h3 class="banner__title">{{ bannerData.title }}</h3>
-      <p class="banner__description">{{ bannerData.description }}</p>
-    </div>
+    <LoaderComponent :data="bannerData">
+      <template #template>
+        <ElSkeletonItem class="banner__container" variant="rect" />
+      </template>
+      <template #default>
+        <div
+          class="banner__container"
+          :style="{ backgroundImage: `url(${bannerData[bannerKey].image})` }"
+        >
+          <h3 class="banner__title">{{ bannerData[bannerKey].title }}</h3>
+          <p class="banner__description">{{ bannerData[bannerKey].description }}</p>
+        </div>
+      </template>
+    </LoaderComponent>
   </section>
 </template>
 
 <style scoped lang="scss">
-  .banner {
-    &__container {
-      background-size: cover;
-      background-repeat: no-repeat;
-    }
-    &__title {
-      max-width: 328px;
-      font: 700 38px/52px Montserrat;
-      color: var(--title-color);
-      margin-bottom: 7px;
-    }
-    &__description {
-      font: 500 16px/52px Montserrat;
-      color: var(--banner-color);
-    }
-    &__container {
-      max-width: 1206px;
-      width: calc(100% - 30px);
-      margin: 0 auto;
-      min-height: 395px;
-      border-radius: 12px;
-      padding-top: 70px;
-      padding-left: 55px;
-    }
+.banner {
+  &__title {
+    max-width: 328px;
+    font: 700 38px/52px Montserrat;
+    color: var(--title-color);
+    margin-bottom: 7px;
   }
+  &__description {
+    font: 500 16px/52px Montserrat;
+    color: var(--banner-color);
+  }
+  &__container {
+    display: block;
+    max-width: 1206px;
+    width: calc(100% - 30px);
+    margin: 0 auto;
+    min-height: 395px;
+    border-radius: 12px;
+    padding-top: 70px;
+    padding-left: 55px;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+}
 </style>
