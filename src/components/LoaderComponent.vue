4fe5ee1f548cb8a85ba2eb3cae77d25806e01a53
Bulation <insanbulat@gmail.com>
2023-05-07 13:25:56 +0300

    feat: add loader component

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+import { ElSkeleton } from 'element-plus'
+
+const isLoading = ref(true)
+
+watchEffect(() => {
+  if (props.data) {
+    isLoading.value = false
+  }
+})
+
+const props = defineProps<{
+  data: any
+  count?: number
+}>();
+</script>
+
+<template>
+  <ElSkeleton :style="'position: relative'" :loading="isLoading" animated :count="count">
+    <template #template>
+      <slot name="template"></slot>
+    </template>
+    <template #default>
+      <slot name="default"></slot>
+    </template>
+  </ElSkeleton>
+</template>
