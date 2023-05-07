7f7daff2f2c539ea172b214f62c4affd2722376a
Bulation <insanbulat@gmail.com>
2023-05-07 13:22:51 +0300

    feat: add metainfo and keep-alive

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import PageHeader from '@/layout/PageHeader.vue'
+</script>
+
+<template>
+  <metainfo>
+    <template v-slot:title="{ content }">{{ content }}</template>
+    <template v-slot:description="{ content }">{{ content.description }}</template>
+    <template v-slot:keywords="{ content }">{{ content }}</template>
+    <template v-slot:og(title)="{ content }">
+      {{ content }}
+    </template>
+    <template v-slot:og(description)="{ content }">
+      {{ content }}
+    </template>
+  </metainfo>
+  <PageHeader />
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
