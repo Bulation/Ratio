c2b9c45b8e52dfd82571e75e28be37a066acf44c
Bulation <insanbulat@gmail.com>
2023-05-07 13:24:39 +0300

    feat: add error component and meta

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,63 +1,83 @@
 <script setup lang="ts">
-import SearchSection from './SearchSection.vue';
-import LatestSection from './LatestSection.vue';
-import BannerComponent from '@/components/BannerComponent.vue';
-import PageFooter from '@/layout/PageFooter.vue';
-import API from '@/services/api';
-import { onMounted, ref } from 'vue';
-import type { IBannerData } from '@/interfaces/IBannerData';
-import FeaturedSection from './FeaturedSection.vue';
-import type { ILatestHotelData } from '@/interfaces/ILatestHotelData';
-import type { IHotelData } from '@/interfaces/IHotelData';
+import SearchSection from './SearchSection.vue'
+import LatestSection from './LatestSection.vue'
+import BannerComponent from '@/components/BannerComponent.vue'
+import PageFooter from '@/layout/PageFooter.vue'
+import API from '@/services/api'
+import { onMounted, ref } from 'vue'
+import type { IBannerData } from '@/interfaces/IBannerData'
+import FeaturedSection from './FeaturedSection.vue'
+import type { ILatestHotelData } from '@/interfaces/ILatestHotelData'
+import type { IHotelData } from '@/interfaces/IHotelData'
+import ErrorComponent from '@/components/ErrorComponent.vue'
+import { useMeta } from 'vue-meta';
 
-const banners = ref<IBannerData>();
-const latestList = ref<ILatestHotelData[]>([]);
-const featuredList = ref<IHotelData[]>([]);
+useMeta({
+  title: "Home",
+  meta: [
+    { name: 'description', content: "Order and research our list of hotels" },
+    { name: 'keywords', content: "hotel, hotel list, order, preserve hotel" }
+  ],
+  og: {
+    title: "Hotel",
+    keywords: "hotel, hotel list, order, preserve hotel",
+    description: "Order and research our list of hotels",
+  },
+});
+
+const banners = ref<IBannerData>(null)
+const latestList = ref<ILatestHotelData[]>(null)
+const featuredList = ref<IHotelData[]>(null)
+
+const isError = ref(false);
 
 onMounted(() => {
-  getBanners();
-  getLatestList();
-  getFeaturedList();
+  getBanners()
+  getLatestList()
+  getFeaturedList()
 })
 
 const getBanners = async () => {
   try {
-    banners.value = await API.getBanners();
+    banners.value = await API.getBanners()
   } catch (e) {
-    // render Error Component
+    isError.value = true;
   }
 }
 
 const getLatestList = async () => {
   try {
-    latestList.value = await API.getLatestData();
+    latestList.value = await API.getLatestData()
   } catch (e) {
-    // render Error Component
+    isError.value = true;
   }
 }
 
 const getFeaturedList = async () => {
   try {
-    featuredList.value = await API.getFeaturedData();
+    featuredList.value = await API.getFeaturedData()
   } catch (e) {
-    // render Error Component
+    isError.value = true;
   }
 }
 </script>
 
 <template>
-  <main v-if="banners">
-    <SearchSection :imageUrl="banners.main.image" />
+  <template v-if="isError">
+    <ErrorComponent />
+  </template>
+  <main>
+    <SearchSection :bannerData="banners" />
     <LatestSection :latest-list="latestList" />
-    <BannerComponent :bannerData="banners.second" />
+    <BannerComponent :bannerData="banners" bannerKey="second" />
     <FeaturedSection :featured-list="featuredList" />
-    <BannerComponent :bannerData="banners.last" />
+    <BannerComponent :bannerData="banners" bannerKey="last" />
   </main>
   <PageFooter />
 </template>
 
 <style scoped>
-  main {
-    padding-bottom: 57px;
-  }
+main {
+  padding-bottom: 57px;
+}
 </style>
