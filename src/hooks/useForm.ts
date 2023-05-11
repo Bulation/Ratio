206a9c465c8b33d1f894de57f6b51c14e6864a52
Bulation <insanbulat@gmail.com>
2023-05-11 23:31:28 +0300

    refactor: move hooks to src

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,31 @@
+import { ref } from 'vue'
+
+export default function useForm(
+  formState: Record<string, any>,
+  errors: Record<string, boolean>,
+  rules: Record<string, (value: string) => boolean>
+) {
+  const isSubmitDisabled = ref(false)
+  const clearError = (errorKey: string) => {
+    isSubmitDisabled.value = false
+    if (errors[errorKey] !== undefined) {
+      errors[errorKey] = false
+    }
+  }
+
+  const validateForm = () => {
+    Object.keys(errors).forEach((key) => {
+      const isError = rules[key](formState[key])
+      errors[key] = isError
+      if (!isSubmitDisabled.value && isError) {
+        isSubmitDisabled.value = true
+      }
+    })
+  }
+
+  const updateFormState = (value: string, name: string) => {
+    formState[name] = value
+    clearError(name)
+  }
+  return { isSubmitDisabled, clearError, updateFormState, validateForm }
+}
