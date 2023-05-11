c1f0c582895a5df2904cf878168a1829df47b026
Bulation <insanbulat@gmail.com>
2023-05-11 14:37:33 +0300

    refactor: remove metainfo

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,6 @@ import PageHeader from '@/layout/PageHeader.vue'
 </script>
 
 <template>
-  <metainfo>
-    <template v-slot:title="{ content }">{{ content }}</template>
-    <template v-slot:description="{ content }">{{ content.description }}</template>
-    <template v-slot:keywords="{ content }">{{ content }}</template>
-    <template v-slot:og(title)="{ content }">
-      {{ content }}
-    </template>
-    <template v-slot:og(description)="{ content }">
-      {{ content }}
-    </template>
-  </metainfo>
   <PageHeader />
   <router-view v-slot="{ Component }">
     <keep-alive>
