40742d03c3fa2ef9d2f0c37116d13fad9cd20645
Bulation <insanbulat@gmail.com>
2023-05-11 14:42:15 +0300

    feat: add map

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="center">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+</template>
+
+<script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+const zoom = 9;
+interface IMapProps {
+  center: string[];
+}
+
+defineProps<IMapProps>()
+
+</script>
