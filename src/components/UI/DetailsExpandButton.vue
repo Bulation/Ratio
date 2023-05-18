1577c8e7530c89044a3247d913bcda514616ab8a
Bulation <insanbulat@gmail.com>
2023-05-18 23:25:10 +0300

    feat: add button for details page

---
+++ src/components/UI/DetailsExpandButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="details-expand-button" type="button"><slot></slot></button>
+</template>
+
+<style scoped lang="scss">
+.details-expand-button {
+  font: 600 15px/18px Montserrat;
+  color: var(--main-text-color);
+  padding: 20px 35px;
+  border: 1px solid var(--main-text-color);
+  border-radius: 6px;
+  background-color: #ffffff;
+  cursor: pointer;
+}
+</style>
