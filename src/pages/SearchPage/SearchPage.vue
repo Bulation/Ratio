4f0685eb339aafc470186f65e0749a245655361d
Bulation <insanbulat@gmail.com>
2023-05-18 21:34:48 +0300

    feat: separate code to different modules

--- src/pages/SearchPage/SearchPage.vue
+++ src/pages/SearchPage/SearchPage.vue
@@ -1,24 +1,20 @@
 <script setup lang="ts">
-import LoaderComponent from '@/components/LoaderComponent.vue';
-import MapComponent from '@/components/MapComponent.vue';
+import SearchMap from './SearchMap.vue';
 import SearchHotelsList from './SearchHotelsList.vue';
-import SearchAboutHotel from './SearchAboutHotel.vue';
-import { ElSkeletonItem } from 'element-plus';
 import API from '@/services/api';
-import { useSearchedState } from '@/store';
-import { onActivated, ref, watch } from 'vue';
+import { useSearchedState } from '@/store/modules/searchModule';
 import type { IHotelData } from '@/interfaces/IHotelData';
 import type { IDetailedHotelData } from '@/interfaces/IDetailedHotelData';
 import { useHead } from 'unhead'
+import { onActivated, ref, watch } from 'vue';
+
 const store = useSearchedState();
-const list = ref<IHotelData[]>(null)
-const copyList = ref<IHotelData[]>(null);
+const list = ref<IHotelData[]>([])
 const firstHotel = ref<IDetailedHotelData>(null);
 
 const getList = async () => {
     list.value = await API.postFilter(store.getSearchedState);
     firstHotel.value = await API.getHotelData(list.value[0]._id);
-    copyList.value = list.value.slice(0, 3);
 }
 
 watch(list, () => {
@@ -33,30 +29,16 @@ watch(list, () => {
   })
 })
 
-onActivated(async () => { 
+onActivated(async () => {
   await getList();
 });
-
-const showMore = () => {
-  copyList.value = list.value;
-}
 
 </script>
 
 <template>
   <section class="search">
-    <SearchHotelsList :list="list" :copyList="copyList" @showMore="showMore" />
-    <LoaderComponent :style="'width: auto;'" :data="firstHotel">
-      <template #template>
-        <ElSkeletonItem :style="'display: block; width: 668px; height: 736px;'" variant="rect" />
-      </template>
-      <template #default>
-        <div class="map">
-          <MapComponent :center="firstHotel.coords" />
-          <SearchAboutHotel :name="firstHotel.name" :address="firstHotel.address" :amenities="firstHotel.amenities" />
-        </div>
-      </template>
-    </LoaderComponent>
+    <SearchHotelsList :list="list" />
+    <SearchMap :firstHotel="firstHotel" />
   </section>
 </template>
 
@@ -72,11 +54,4 @@ const showMore = () => {
     display: block;
   }
 }
-.map {
-  position: relative;
-  width: 100%;
-  max-width: 668px;
-  height: 736px;
-}
-
 </style>
