9ca74073bbf383439a3ac4975854dc2f2f7f69d3
Bulation <insanbulat@gmail.com>
2023-05-18 21:30:16 +0300

    feat: add service for work with metrika

---
+++ src/services/yandexMetrika.ts
@@ -0,0 +1,21 @@
+import { YANDEX_METRICA_ID } from "@/constants"
+
+type TargetName = 'location' | 'checkIn' | 'checkOut' | 'guest' | 'orderError';
+
+function isTargetName(value: string): value is TargetName {
+  return ['location', 'checkIn', 'checkOut', 'guest', 'orderError'].includes(value)
+}
+
+const yandexMetrica = {
+  setGoal(targetName: string) {
+    if (!isTargetName(targetName)) {
+      return;
+    }
+    window['ym' as keyof typeof window](YANDEX_METRICA_ID, 'reachGoal', targetName)
+  },
+  redirectToPage(url: string) {
+    window['ym' as keyof typeof window](YANDEX_METRICA_ID, 'hit', url);
+  }
+}
+
+export default yandexMetrica;
